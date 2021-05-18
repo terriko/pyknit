b41059d737da10e4488670db8b8099865a0d36ad
jinder1s <msingh@edx.org>
2021-05-18 11:34:46 -0400

    squash: running black on files

--- pyknit/pi_shawl.py
+++ pyknit/pi_shawl.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 import argparse
 
+
 def total_rounds_for_pi_shawl(desired_radius: float, round_gauge: float) -> int:
     """Returns number of rounds necessary to create a pi shawl of given radius"""
-    return round(desired_radius* round_gauge)
+    return round(desired_radius * round_gauge)
+
 
 def pi_shawl_increase_rows(desired_radius: float, round_gauge: float) -> str:
     "returns a list with the number of round at which to double the stitches"
     num_rounds_for_pi_shawl = total_rounds_for_pi_shawl(desired_radius, round_gauge)
     num_of_rounds_before_increase_step = 3
-    increase_rows = [2] # increase on first round after cast-on
+    increase_rows = [2]  # increase on first round after cast-on
     num_round = 2
     while num_round <= num_rounds_for_pi_shawl:
         num_rounds_since_last_increase_row = num_round - increase_rows[-1]
@@ -21,11 +23,12 @@ def pi_shawl_increase_rows(desired_radius: float, round_gauge: float) -> str:
 
 
 def main():
-    parser = argparse.ArgumentParser(description = "Pi shawl calculations")
-    parser.add_argument('desired_radius', type=float, help='Radius of your pi shawl')
-    parser.add_argument('round_gauge', type=float, help="TODO")
+    parser = argparse.ArgumentParser(description="Pi shawl calculations")
+    parser.add_argument("desired_radius", type=float, help="Radius of your pi shawl")
+    parser.add_argument("round_gauge", type=float, help="TODO")
     args = parser.parse_args()
     print(pi_shawl_increase_rows(args.desired_radius, args.round_gauge))
+
 
 if __name__ == "__main__":
     main()
--- test/test_pi_shawl.py
+++ test/test_pi_shawl.py
@@ -2,7 +2,6 @@
 from pyknit import pi_shawl
 
 
-
 def test_pi_shawl_increase_rows():
     assert pi_shawl.pi_shawl_increase_rows(5, 5) == [2, 6, 13]
     assert pi_shawl.pi_shawl_increase_rows(50, 3) == [2, 6, 13, 26, 51, 100]
