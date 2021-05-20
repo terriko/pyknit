8c8643efe77eb46712172100669b483334dfa246
Ben Hampson <bjhampson@gmail.com>
2021-05-21 00:14:34 +0100

    add pydantic data validation to GaugeSwatch

--- pyknit/GaugeSwatch.py
+++ pyknit/GaugeSwatch.py
@@ -10,26 +10,18 @@ pyKnit.GaugeSwatch: Tools for measurement and gauge swatching
 """
 
 import math
-from typing import Set
+from typing import Set, Literal
+from pydantic import BaseModel, PositiveInt, PositiveFloat, validate_arguments
 
 
-class GaugeSwatch:
+class GaugeSwatch(BaseModel):
     """Information from a gauge swatch"""
-
-    def __init__(
-        self,
-        row_count: int,
-        row_measure: float,
-        stitch_count: int,
-        stitch_measure: float,
-        units: str,
-    ):
-        self.row_count = row_count
-        self.row_measure = row_measure
-        self.stitch_count = stitch_count
-        self.stitch_measure = stitch_measure
-        self.units = units
-        # TODO: add yardage/weight for calculations?
+    row_count: PositiveInt
+    row_measure: PositiveFloat
+    stitch_count: PositiveInt
+    stitch_measure: PositiveFloat
+    units: Literal['cm', 'in']
+    # TODO: add yardage/weight for calculations?
 
     def row_gauge(self) -> float:
         """ return rows per unit (e.g. cm, inch) number """
@@ -39,24 +31,28 @@ class GaugeSwatch:
         """ return stitches per unit (e.g. cm, inch) number """
         return self.stitch_count / self.stitch_measure
 
-    def measurement_to_stitches(self, measurement: float) -> int:
+    @validate_arguments
+    def measurement_to_stitches(self, measurement: PositiveFloat) -> int:
         """
         Given a measurement, how many stiches would we need?
         Round to closest stitch.
         """
         return round(measurement * self.stitch_gauge())
 
-    def measurement_to_rows(self, measurement: float) -> int:
+    @validate_arguments
+    def measurement_to_rows(self, measurement: PositiveFloat) -> int:
         """
         Given a measurement, how many rows would we need?
         Round to closest number of rows."""
         return round(measurement * self.row_gauge())
 
-    def rows_to_measurement(self, rows: int) -> float:
+    @validate_arguments
+    def rows_to_measurement(self, rows: PositiveInt) -> float:
         """ figure out how long a number of rows will be """
         return rows / self.row_gauge()
 
-    def stitches_to_measurement(self, stitches: int) -> float:
+    @validate_arguments
+    def stitches_to_measurement(self, stitches: PositiveInt) -> float:
         """ figure out how wide a number of stitches will be """
         return stitches / self.stitch_gauge()
 
