06d489a315aa4b7f515091abed44896d15844861
Tom Wilson <tom.andrew.wilson@gmail.com>
2022-04-27 21:07:36 -0400

    add filename and linenumber to log format

--- pyknit/__init__.py
+++ pyknit/__init__.py
@@ -10,7 +10,7 @@ logging_config_dict = dict(
     version=1,
     formatters={
         "simple": {
-            "format": """%(asctime)s | %(name)-12s | %(levelname)-8s | %(message)s"""
+            "format": """%(asctime)s | %(filename)s | %(lineno)d | %(levelname)s | %(message)s"""
         }
     },
     handlers={"console": {"class": "logging.StreamHandler", "formatter": "simple"}},
