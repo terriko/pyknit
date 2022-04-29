491d05f9f6357e7c8744daef0137e3962b3ff5dd
Alan <alan.bloomer@gmail.com>
2022-04-29 15:28:59 +0100

    added row numbers, fixed type hints

--- pyknit/Chart.py
+++ pyknit/Chart.py
@@ -9,54 +9,20 @@ patterns and more
 pyKnit.Chart: chart and pattern parsing functions
 """
 
+from posixpath import relpath
 import re
 from PIL import Image, ImageDraw, ImageFont
-from typing import List, Set
+from typing import Dict, List, Sequence, Set
+import os.path, os
 
-stitch_legend = {  # Default legend. Incomplete for now.
-    "k": {
-        "instruction": "knit",
-        "symbol": " ",
-        "width": 1,  
-    },
-    "kfb": {
-        "instruction": "knit front and back",
-        "symbol": "V",
-        "width": 1,
-    },
-    "k2tog": {
-        "instruction": "knit two together",
-        "symbol": "/",
-        "width": 1,
-    },
-    "yo": {
-        "instruction": "yarn over",
-        "symbol": "O",
-        "width": 1
-    },
-    "p": {
-        "instruction": "purl", 
-        "symbol": ".",
-        "width": 1,
-    },
-    "ssk": {
-        "instruction": "slip slip knit", # left-leaning decrease 
-        "symbol": "\\",
-        "width": 1,
-    },
-}
 
 class Stitch:
     """A class to represent a stitch. Optionally, a preferred legend can be passed in."""
-    def __init__(self, stitch_name: str, legend: Set[str]=stitch_legend): 
-        if stitch_name not in legend:
-            raise KeyError(f"Stitch '{stitch_name}' not found in legend.")
 
-        stitch_info = legend[stitch_name]
-
-        self.instruction = stitch_info["instruction"]
-        self.symbol = stitch_info["symbol"]
-        self.width = stitch_info["width"]
+    def __init__(self, instruction: str, symbol: str, width: int):
+        self.instruction = instruction
+        self.symbol = symbol
+        self.width = width
 
     def __repr__(self):
         return f"'{self.symbol}'"
@@ -68,12 +34,40 @@ class Stitch:
         if isinstance(other, Stitch):
             return self.__dict__ == other.__dict__
         return False
-            
 
+
+Legend = Dict[str, Stitch]
+PatternRow = List[Stitch]
+Pattern = List[PatternRow]
+
+stitch_legend = {  # Default legend. Incomplete for now.
+    "k": Stitch(instruction="knit", symbol=" ", width=1),
+    "kfb": Stitch(
+        instruction="knit front and back",
+        symbol="V",
+        width=1,
+    ),
+    "k2tog": Stitch(
+        instruction="knit two together",
+        symbol="/",
+        width=1,
+    ),
+    "yo": Stitch(instruction="yarn over", symbol="O", width=1),
+    "p": Stitch(
+        instruction="purl",
+        symbol=".",
+        width=1,
+    ),
+    "ssk": Stitch(
+        instruction="slip slip knit",  # left-leaning decrease
+        symbol="\\",
+        width=1,
+    ),
+}
 ## Chart and pattern parsing functions
 
 
-def parse_row(row: str, legend: Set[str]=stitch_legend) -> Set[str]:
+def parse_row(row: str, legend: Legend = stitch_legend) -> List[str]:
     # I don't think a set is the right return type, order is important here
     """Parse a written set of knitting instructions and print an array of
     stitches using a legend.  This is a stand in for eventually printing a
@@ -97,23 +91,24 @@ def parse_row(row: str, legend: Set[str]=stitch_legend) -> Set[str]:
                 # set the number or if no number, assume you repeat once
                 number = int(result.group(2)) if result.group(2) else 1
                 for i in range(0, number):
-                    stitch_array.append(Stitch(stitch, legend))
+                    stitch_array.append(legend[stitch])
 
     return stitch_array
 
 
-def parse_chart(chart_instructions:str, legend: Set[str]=stitch_legend) -> List[List[Stitch]]:
-    return [parse_row(row) for row in chart_instructions.split("\n")]
+def parse_chart(chart_instructions: str, legend: Legend = stitch_legend) -> Pattern:
+    return [parse_row(row, legend) for row in chart_instructions.split("\n")]
 
 
-def print_row(stitch_array: Set[str]) -> Image: 
-    """ Print a chart from a stitch array """
-    # TODO do a 2D chart
+def print_row(stitch_array: PatternRow) -> Image:
+    """Print a chart from a stitch array"""
     # Set up the image
     cell_height = 50
     cell_width = 50
     chart_image = Image.new(
-        "RGB", ((cell_width + 1) * sum(st.width for st in stitch_array), cell_height), (200, 200, 200)
+        "RGB",
+        ((cell_width + 1) * sum(st.width for st in stitch_array), cell_height),
+        (200, 200, 200),
     )
 
     # draw some gridlines
@@ -124,49 +119,64 @@ def print_row(stitch_array: Set[str]) -> Image:
     position = 0
     for i, stitch in enumerate(stitch_array):
 
-    # for i in range(1, len(stitch_array)):
-        position = sum(st.width for st in stitch_array[:i+1]) * (cell_width + 1)
-        draw.line(
-            (position, 0) + (position, cell_height), fill=128
-        )
+        # for i in range(1, len(stitch_array)):
+        position = sum(st.width for st in stitch_array[: i + 1]) * (cell_width + 1)
+        draw.line((position, 0) + (position, cell_height), fill=128)
 
-    
         draw.text(
-            (position - (stitch.width * cell_width)/2, cell_height/2),
-            stitch.symbol, font=fnt,
+            (position - (stitch.width * cell_width) / 2, cell_height / 2),
+            stitch.symbol,
+            font=fnt,
             fill=(255, 255, 255, 255),
             align="center",
-            anchor="mm"
+            anchor="mm",
         )
     return chart_image
 
-def instruction_to_plot_order(input_array:str, vertical_order:str="bt", horizontal_order:str="rl"):
+
+def instruction_to_plot_order(
+    input_array: Pattern, vertical_order: str = "bt", horizontal_order: str = "rl"
+) -> Pattern:
     # input_array = [list(row) for row in pattern.lstrip().rstrip().split("\n")]
-    vertical_ordered = list(reversed(input_array)) if vertical_order == "bt" else input_array
-    return_array = [list(reversed(row)) if horizontal_order == "rl" else row
-        for row in vertical_ordered 
+    vertical_ordered = (
+        list(reversed(input_array)) if vertical_order == "bt" else input_array
+    )
+    return_array = [
+        list(reversed(row)) if horizontal_order == "rl" else row
+        for row in vertical_ordered
     ]
     return return_array
 
 
-def plot_chart(stitch_array: List[List[str]], lr_direction:str="lr", tb_direction:str="tb") -> Image: 
-    """ Print a chart from a stitch array """
-    # TODO do a 2D chart
+def plot_chart(
+    stitch_array: Pattern, lr_direction: str = "lr", tb_direction: str = "tb"
+) -> Image:
+    """Print a chart from a stitch array"""
+
     # Set up the image
     cell_height = 50
     cell_width = 50
 
-    num_rows = len(stitch_array)
-    if num_rows <=0:
+    num_rows = len(stitch_array) if type(stitch_array[0] == list) else 1
+    if num_rows <= 0:
         raise ValueError("There must be at least one row in the pattern")
-    
+    elif num_rows == 1:
+        stitch_array = [
+            stitch_array
+        ]  # put a single row into a containing list to make the 2D loop work
+
     longest_row_len = max([sum(st.width for st in row) for row in stitch_array])
 
     print(f"{num_rows} rows, {longest_row_len} sts wide at max")
-    sts_to_plot = instruction_to_plot_order(stitch_array, tb_direction,lr_direction)
+    pattern_to_plot = instruction_to_plot_order(
+        stitch_array, tb_direction, lr_direction
+    )
 
+    # Set up canvas with room for all stitches plus numbers
     chart_image = Image.new(
-        "RGB", (cell_width * longest_row_len, cell_height*num_rows), (200, 200, 200)
+        "RGB",
+        (cell_width * (longest_row_len + 1), cell_height * (num_rows + 1)),
+        (200, 200, 200),
     )
 
     # draw some gridlines
@@ -176,26 +186,88 @@ def plot_chart(stitch_array: List[List[str]], lr_direction:str="lr", tb_directio
     fnt = ImageFont.truetype("cour.ttf", 35)
     color_st_pattern = r"#[0-9a-fA-F]{6}"
 
-    for st_y, row in enumerate(sts_to_plot):
-        cur_y = st_y * cell_height
-        cur_x = 0
+    for st_y, row in enumerate(pattern_to_plot):
+        cur_y = (st_y + (1 if tb_direction == "tb" else 0)) * cell_height
+        cur_x = 0 if lr_direction == "rl" else cell_width
         for st_x, stitch in enumerate(row):
-        # for i in range(1, len(stitch_array)):
+            # for i in range(1, len(stitch_array)):
             stitch_coloured = re.match(color_st_pattern, stitch.symbol)
-            stitch_color = stitch.symbol if stitch_coloured else "white"
-            draw.rectangle(
-                ((cur_x, cur_y), (cur_x+stitch.width*cell_width, cur_y+cell_height)),
-                fill=stitch_color,
-                outline="black"
-            )
-            if not stitch_coloured:
-                draw.text(
-                    (cur_x + (stitch.width * cell_width)/2, cur_y + cell_height/2),
-                    stitch.symbol, font=fnt,
-                    fill="blue",
-                    align="center",
-                    anchor="mm"
+            stitch_graphic = stitch.symbol.endswith(".png")
+
+            if stitch_graphic:
+                with Image.open(
+                    os.path.normpath(
+                        os.path.join(
+                            os.path.dirname(os.path.abspath(__file__)),
+                            "symbols",
+                            stitch.symbol,
+                        )
+                    )
+                ) as sym:
+                    chart_image.paste(sym, (cur_x, cur_y))
+
+            else:
+                stitch_color = stitch.symbol if stitch_coloured else "white"
+                draw.rectangle(
+                    (
+                        (cur_x, cur_y),
+                        (cur_x + stitch.width * cell_width, cur_y + cell_height),
+                    ),
+                    fill=stitch_color,
+                    outline="black",
                 )
-            cur_x +=stitch.width*cell_width
+                if not stitch_coloured:
+                    draw.text(
+                        (
+                            cur_x + (stitch.width * cell_width) / 2,
+                            cur_y + cell_height / 2,
+                        ),
+                        stitch.symbol,
+                        font=fnt,
+                        fill="blue",
+                        align="center",
+                        anchor="mm",
+                    )
+            cur_x += stitch.width * cell_width
+    # Row and column numbers
+    row_x = (
+        3 * cell_width // 2
+        if lr_direction == "lr"
+        else chart_image.width - 3 * cell_width // 2
+    )
+    row_y = (
+        cell_height // 2
+        if tb_direction == "tb"
+        else chart_image.height - cell_height // 2
+    )
+    for i in range(1, longest_row_len + 1):
+        draw.text(
+            (row_x, row_y),
+            str(i),
+            fill="black",
+            font=fnt,
+            align="center",
+            anchor="mm",
+        )
+        row_x = row_x + cell_width * (1 if lr_direction == "lr" else -1)
+
+    col_x = (
+        cell_width // 2 if lr_direction == "lr" else chart_image.width - cell_width // 2
+    )
+    col_y = (
+        3 * cell_height // 2
+        if tb_direction == "tb"
+        else chart_image.height - 3 * cell_height // 2
+    )
+    for j in range(1, num_rows + 1):
+        draw.text(
+            (col_x, col_y),
+            str(j),
+            fill="black",
+            font=fnt,
+            align="center",
+            anchor="mm",
+        )
+        col_y = col_y + cell_height * (1 if tb_direction == "tb" else -1)
 
     return chart_image
