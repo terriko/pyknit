9b1b9a8911aafb601a4e9001e13bb450d1c3c23a
Kenzie Rasmussen <Kenzie.Rasmussen@aa.com>
2022-10-06 19:14:42 -0500

    Japanese charts (#49)
    * Add intellij files to gitignore
    
    * Update .gitignore
    
    * Added basic japanese symbol
    
    includes most single stitch chart markers I could think of, or commonly see in patterns
    
    * formatting
    
    * Updated the Chart.yaml to use the basic symbols
    
    tried to standardize the code formatting to keep it neat to. I know I'm adding a lot of lines.
    
    I also found a way to make the stitch legend mutable so I can just use the methods already there and replace with my stitch legend instead. Kudos to the previous author for making that an option
    
    * Added documentation with sample usage

--- test/test_GaugeSwatch.py
+++ test/test_GaugeSwatch.py
@@ -1,13 +1,11 @@
 # Copyright (C) 2021 Terri Oda
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-#!python
+# !python
 
 import pytest
-import pyknit
+
 from pyknit.GaugeSwatch import GaugeSwatch
-import math
-from typing import Set
 
 
 def test_init():
@@ -19,28 +17,33 @@ def test_init():
     assert gs.stitch_measure == 4
     assert gs.units == "in"
 
+
 @pytest.fixture
 def example_gauge_swatches():
     gs_good_1 = GaugeSwatch(row_count=22, row_measure=3.75, stitch_count=18, stitch_measure=4, units="in")
     gs_good_2 = GaugeSwatch(row_count=18, row_measure=3.25, stitch_count=24, stitch_measure=4, units="in")
     return [gs_good_1, gs_good_2]
 
+
 def test_row_gauge(example_gauge_swatches):
-    expected = [22/3.75, 18/3.25]
+    expected = [22 / 3.75, 18 / 3.25]
     computed = [gs.row_gauge() for gs in example_gauge_swatches]
     assert computed == expected
 
+
 def test_stitch_gauge(example_gauge_swatches):
-    expected = [18/4, 24/4]
+    expected = [18 / 4, 24 / 4]
     computed = [gs.stitch_gauge() for gs in example_gauge_swatches]
     assert computed == expected
+
 
 @pytest.fixture
 def swatch():
     return GaugeSwatch(row_count=18, row_measure=3.25, stitch_count=24, stitch_measure=4, units="in")
 
+
 class TestGaugeSwatch:
-    
+
     def test_row_gauge(self, swatch):
         assert swatch.row_gauge() == 18 / 3.25
 
