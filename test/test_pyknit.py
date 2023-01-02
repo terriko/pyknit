a0a1e1a6e506c4da056cf58d503f8059e7c7a858
wilsonify <tom.andrew.wilson@gmail.com>
2023-01-02 16:51:26 -0500

    implement test for decrease_evenly

--- test/test_pyknit.py
+++ test/test_pyknit.py
@@ -80,3 +80,43 @@ def test_increase_evenly_error(starting_count, increase_number, in_the_round, ex
     with pytest.raises(expected):
         logging.info(f"this should raise a valueError")
         pyknit.increase_evenly(starting_count, increase_number, in_the_round)
+
+
+@pytest.mark.parametrize(
+    ("starting_count", "decrease_number", "in_the_round", "expected"),
+    [
+        (11, 3, True, "k2, k2tog, k1, k2tog, k2, k2tog"),
+        (11, 3, False, "k1, k2tog, k2, k2tog, k2, k2tog"),
+
+        (19, 5, True, "[k2, k2tog] * 2 times, k1, k2tog, [k2, k2tog] * 2 times"),
+        (19, 5, False, "k1, [k2tog, k2] * 2 times, [k2tog, k2] * 2 times, k2tog"),
+
+        (20, 5, True, "[k2, k2tog] * 5 times"),
+        (20, 5, False, "k1, [k2tog, k2] * 4 times, k2tog, k1"),
+
+        (21, 5, True, "[k2, k2tog] * 2 times, k3, k2tog, [k2, k2tog] * 2 times"),
+        (21, 5, False, "k2, [k2tog, k2] * 2 times, [k2tog, k2] * 2 times, k2tog k1"),
+
+    ],
+)
+def test_decrease_evenly(starting_count, decrease_number, in_the_round, expected):
+    assert pyknit.decrease_evenly(
+        starting_count=starting_count,
+        decrease_number=decrease_number,
+        in_the_round=in_the_round
+    ) == expected
+
+
+@pytest.mark.parametrize(
+    ("starting_count", "decrease_number", "in_the_round", "expected"),
+    [
+        (1, 2, True, ValueError),
+        (2, 3, False, ValueError),
+        (10, 7, True, ValueError),
+        (10, 7, False, ValueError),
+    ],
+)
+def test_decrease_evenly_error(starting_count, decrease_number, in_the_round, expected):
+    with pytest.raises(expected):
+        logging.info(f"this should raise a valueError")
+        pyknit.decrease_evenly(starting_count, decrease_number, in_the_round)
